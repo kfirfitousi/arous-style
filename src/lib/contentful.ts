ea201df506915da015fb1bb7d01973f8df810bb3
kfirfitousi <kfirp84@gmail.com>
2022-09-06 21:19:15 +0300

    Fix env variables

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -4,8 +4,8 @@ import { contentfulLoader as cfLoader } from '@delicious-simplicity/next-image-c
 import { createClient, CreateClientParams } from 'contentful';
 
 const config: CreateClientParams = {
-    space: 'ywc3ioqwvbsr',
-    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || '0'
+    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID!,
+    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN!
 };
 
 export const client = createClient(config).withAllLocales;
