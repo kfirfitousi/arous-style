3362e8bd7adfdcfcd3614660c07d0f831eba9734
kfirfitousi <kfirp84@gmail.com>
2022-09-08 15:51:14 +0300

    Give links underline on hover

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -6,7 +6,7 @@ import InstagramSVG from 'public/instagram.svg';
 const Socials = () => {
     return (
         <div className="flex flex-row flex-wrap justify-center mt-5 text-teal-800">
-            <a href="tel:+97236573714" className="flex flex-row items-center space-x-1 px-4">
+            <a href="tel:+97236573714" className="flex flex-row items-center space-x-1 px-4 hover:underline">
                 <PhoneIcon className="h-5 w-5" />
                 <span className="text-lg">03-657-2714</span>
             </a>
@@ -14,7 +14,7 @@ const Socials = () => {
                 href="https://www.instagram.com/arouselbahar/"
                 target="_blank"
                 rel="noreferrer"
-                className="flex flex-row items-center space-x-1 px-4"
+                className="flex flex-row items-center space-x-1 px-4 hover:underline"
             >
                 <Image src={InstagramSVG} alt="Instagram" width={18} height={18} />
                 <span className="text-lg">@arouselbahar</span>
@@ -23,14 +23,14 @@ const Socials = () => {
                 href="https://www.facebook.com/arous.elbahar.jaffa/"
                 target="_blank"
                 rel="noreferrer"
-                className="flex flex-row items-center space-x-1 px-4"
+                className="flex flex-row items-center space-x-1 px-4 hover:underline"
             >
                 <Image src={FacebookSVG} alt="Email" width={20} height={20} />
                 <span className="text-lg">fb.com/arous.elbahar.jaffa</span>
             </a>
             <a
                 href="mailto:arouselbaharorg@gmail.com"
-                className="flex flex-row items-center space-x-1 px-4"
+                className="flex flex-row items-center space-x-1 px-4 hover:underline"
             >
                 <EnvelopeIcon className="w-6 h-6" />
                 <span className="text-lg">arouselbaharorg@gmail.com</span>
