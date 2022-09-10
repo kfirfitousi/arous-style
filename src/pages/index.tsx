179140dfe5066bf2eb3e5d51fe2eb1ffaac602d6
kfirfitousi <kfirp84@gmail.com>
2022-09-10 20:57:09 +0300

    Add custom document

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,12 +27,11 @@ const Home: NextPage = () => {
     return (
         <>
             <Head>
-                <title>Arous Style - לקנות בסטייל</title>
-                <meta name="description" content="Arous Style" />
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-                <link rel="manifest" href="/site.webmanifest" />
+                <title>Arous Style • לקנות בסטייל</title>
+                <meta
+                    name="description"
+                    content="Arous Style - Arous Elbahar's second-hand store located in Jaffa, Isreal"
+                />
             </Head>
 
             <main className="h-full md:h-screen w-full flex flex-col-reverse md:flex-row">
